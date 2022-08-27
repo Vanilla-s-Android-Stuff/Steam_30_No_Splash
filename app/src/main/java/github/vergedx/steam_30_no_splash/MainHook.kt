beec4adf5a3dd622b7ffa467ad4084320b56b4a8
Vanilla <osu_Vanilla@126.com>
2022-08-27 10:38:42 +0800

    feat[settings.gradle]: Adding missing maven repo.

--- app/src/main/java/github/vergedx/steam_30_no_splash/MainHook.kt
+++ app/src/main/java/github/vergedx/steam_30_no_splash/MainHook.kt
@@ -1,3 +1,10 @@
 package github.vergedx.steam_30_no_splash
 
-class MainHook
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class MainHook : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
+        TODO("Not yet implemented")
+    }
+}
