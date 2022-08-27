5e72c324766c63c074e17aeeca6bff92a433d0bf
Vanilla <osu_Vanilla@126.com>
2022-08-27 10:53:22 +0800

    feat[MainHook.kt]: Finish hidden splash screen.

--- app/src/main/java/github/vergedx/steam_30_no_splash/MainHook.kt
+++ app/src/main/java/github/vergedx/steam_30_no_splash/MainHook.kt
@@ -1,10 +1,23 @@
 package github.vergedx.steam_30_no_splash
 
+import android.app.Activity
 import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class MainHook : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
-        TODO("Not yet implemented")
+        if (lpparam?.packageName != "com.valvesoftware.android.steam.community") return
+
+        XposedHelpers.findAndHookMethod(
+            "expo.modules.splashscreen.singletons.SplashScreen", lpparam.classLoader,
+            "show", Activity::class.java, "expo.modules.splashscreen.SplashScreenImageResizeMode",
+            Class::class.java, Boolean::class.javaPrimitiveType, object : XC_MethodReplacement() {
+                override fun replaceHookedMethod(param: MethodHookParam) {
+                    return
+                }
+            }
+        )
     }
 }
